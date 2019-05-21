a2221e24eed7510646590274405f8cddb34aa52f
NikoU <niko.uremovic@student.um.si>
2019-05-21 23:41:45 +0200

    hakuna matata

---
+++ autolander.py
@@ -0,0 +1,225 @@
+import numpy
+
+class KalmanFilter(object):
+
+    def __init__(self, process_variance, estimated_measurement_variance):
+        self.process_variance = process_variance
+        self.estimated_measurement_variance = estimated_measurement_variance
+        self.posteri_estimate = 0.0
+        self.posteri_error_estimate = 1.0
+
+    def input_latest_noisy_measurement(self, measurement):
+        priori_estimate = self.posteri_estimate
+        priori_error_estimate = self.posteri_error_estimate + self.process_variance
+
+        blending_factor = priori_error_estimate / (priori_error_estimate + self.estimated_measurement_variance)
+        self.posteri_estimate = priori_estimate + blending_factor * (measurement - priori_estimate)
+        self.posteri_error_estimate = (1 - blending_factor) * priori_error_estimate
+
+    def get_latest_estimated_measurement(self):
+        return self.posteri_estimate
+
+class AutoLander():
+    def __init__(self, objectMass, objectMaxThrust, objectNeutralThrust, timeStep, ttl, snr_dB):
+        '''
+        Inicializacija AutoLander implementacije.
+        Tu prejmete informacije o letalniku.
+
+        objectMass - float
+            masa letalnika v kg
+
+        objectMaxThrust - float
+            maksimalna sila potiska motorjev v N
+
+        objectNeutralThrust - float
+            relativni delež potiska, da letalnik lahko lebdi, med 0 in 1
+        '''
+        self.currentAltitude = 0
+        self.objectMass = objectMass
+        self.objectMaxThrust = objectMaxThrust
+        self.objectNeutralThrust = objectNeutralThrust
+        # compute g
+        a_d = self.objectMaxThrust / self.objectMass
+        Fg = self.objectNeutralThrust * self.objectMaxThrust
+        self.g = Fg / self.objectMass
+        
+        self.timeStep = timeStep
+        self.ttl = ttl # time to land
+
+        self.ts = 0 # time sample (index)
+        self.strategy = 'Niko'
+
+        # Kalman Filter
+        self.snr_dB = snr_dB
+        self.kFilter = None
+
+        # history of (filtered altitudes)
+        self.h_Altitudes = numpy.zeros(int(self.ttl / self.timeStep))
+        # estimated start velocity and altitude
+        self.sAlt = None
+        self.sVel = None
+
+        # prepare velocity computation arrays
+        reactionTime = 0.1
+        self.velocitiesCompute = int(reactionTime / self.timeStep) # number of velocities we can afford to compute to estimate start velocity
+        self.averageVelocities = numpy.zeros(self.velocitiesCompute) # velocities on first few (overlapping) time intervals
+
+        # properties for deceleration decision
+        self.tsDecel = None # time sample to start decelerating
+        self.tsStopDecel = None # time sample to stop decelerating
+        self.stopDecel = 0 # deceleration complete when this reaches 3; increment when altitudes_MA > altitudes_HistMA during decel phase
+        self.altitudes_HistMA = None # moving average of altitudes <self.velocitiesCompute> samples back
+        self.altitudes_MA = None # moving average of altitudes
+
+
+    def addHeightMeasurement(self, time, height):
+        '''
+        Prejemek posamezne meritve.
+        time - float
+            čas zajema, v s
+
+        height - float
+            izmerjena višina v m
+        '''
+        # filter
+        if (self.kFilter is None):
+            SNR = numpy.power(10, (self.snr_dB/10))
+            stdDevi = 2 * height / SNR
+            process_variance = 3*10e-4
+            self.kFilter = KalmanFilter(process_variance, stdDevi)
+
+        self.kFilter.input_latest_noisy_measurement(height)
+        self.h_Altitudes[self.ts] = self.kFilter.get_latest_estimated_measurement()
+        self.currentAltitude = self.h_Altitudes[self.ts]
+
+        # uncomment to ignore filter
+        self.h_Altitudes[self.ts] = height
+        self.currentAltitude = height
+
+        # decision
+        self.currentThrust = 0 # free fall
+
+        if (self.strategy == 'PID'):
+            if (pidController is None):
+                pidController = PID(P, I, D, self.currentAltitude)
+                pidController.setSetPoint(15)
+                pidController.setWindup()
+            thrustCorrection = pidController.update(self.currentAltitude) / objectMaxThrust
+            if (self.ts % 50 == 0):
+                debugBr = True
+            if (numpy.absolute(thrustCorrection) < objectMaxThrust/10):
+                pidController.setSetPoint(self.currentAltitude/2)
+        elif (self.strategy == 'Niko'):
+            '''
+            TODO: 
+            - parametrization of reaction time (greater SNR, greater reaction time)
+            - better filter? Kalman with parametrization? Current one works worse as if there wasn't any :D
+            - * estimation of signal to noise ratio based on measurement variance *
+            IMPROVEMENTS:
+            - combination of calculated stopDecel time and measurements
+            - if we notice low starting altitude (object would fall before we can react), add phase of preemptive levitation
+            - parametrization of landing thrust (higher mass, higher ratio of neutral thrust, higher aimedAltitude, higher ratio again)
+            LARGE FEATURES:
+            - * refactor so that constant and known time intervals of measurements taken are no longer needed *
+            - correction of estimations of start velocity and altitude throughout the free fall stage
+            - recursive decisions, multiple deceleration phases
+            '''
+            # gather first few samples for estimation of start velocity and altitude
+            if (self.ts < self.velocitiesCompute * 2 and self.ts >= self.velocitiesCompute):
+                velocityStep = self.ts * self.timeStep * self.g # substract from velocity based on which ts it was taken in
+                self.averageVelocities[self.ts - self.velocitiesCompute] = (self.h_Altitudes[self.ts-self.velocitiesCompute] - self.currentAltitude) / (self.timeStep * self.velocitiesCompute) - velocityStep
+            # decide deceleration start time sample
+            elif (self.ts == self.velocitiesCompute * 2):
+                # estimate start velocity
+                savffi = - numpy.average(self.averageVelocities) # statistically average velocity on first few intervals
+                self.sVel = savffi - (self.velocitiesCompute*self.timeStep)/2 * self.g
+                # estimate start altitude
+                adjustedAltitudes = numpy.zeros(self.velocitiesCompute)
+                for i in range(self.velocitiesCompute):
+                    tempAlt = self.h_Altitudes[i]
+                    averageVelocityFromStartAltTo_tempAlt_ = self.sVel + self.g * i * self.timeStep/2
+                    tempAlt -= averageVelocityFromStartAltTo_tempAlt_ * i * self.timeStep
+                    adjustedAltitudes[i] = tempAlt
+                self.sAlt = numpy.average(adjustedAltitudes)
+                
+                # decide deceleration start sample
+                # h: height if v0 = 0 and aimedAltitude = 0
+                h = self.sAlt + self.sVel**2 / 2 / self.g
+                aimedAltitude = h / 20
+                h -= aimedAltitude
+
+                # calculate time sample to start slowing down
+                F_d = self.objectMaxThrust - self.objectMass * self.g # Fd = Fmax - Fg
+                a_d = F_d / self.objectMass
+                x = (self.g * h / (self.g + a_d))
+                h_allowTravel = h - x
+
+                timeToStartDecel = numpy.sqrt(2*(h_allowTravel)/self.g)
+                moveTimeStep = self.sVel / self.g / self.timeStep # due to the assumption that v0 = 0 and aimedAltitude = 0
+
+                self.tsDecel = int(timeToStartDecel / self.timeStep + moveTimeStep)
+
+                maxVelocity = timeToStartDecel * self.g
+                decelTime = maxVelocity / a_d
+
+                self.tsStopDecel = int(self.tsDecel + decelTime / self.timeStep) + 1
+
+            # deceleration
+            if (self.tsDecel is not None and self.tsDecel <= self.ts):
+                self.currentThrust = self.objectMaxThrust
+            if (self.tsStopDecel is not None and self.tsStopDecel <= self.ts):
+                self.currentThrust = self.objectNeutralThrust - self.objectNeutralThrust / 15
+
+            # implementation of decision on when to stop decelerating 
+            # done by sampling altitudes and estimating velocity based on moving averages;
+            #     
+            # elif (self.tsDecel is not None and self.ts >= self.tsDecel):
+            #     # Moving average of altitudes
+            #     if (self.altitudes_MA is None or self.altitudes_HistMA is None):
+            #         # Init
+            #         self.altitudes_MA = numpy.average(self.h_Altitudes[self.ts-self.velocitiesCompute:self.ts-1:1])
+            #         if (self.ts-self.velocitiesCompute*2 >= 0):
+            #             self.altitudes_HistMA = numpy.average(self.h_Altitudes[self.ts-self.velocitiesCompute*2:self.ts-self.velocitiesCompute-1:1])
+            #         else:
+            #             partialHistory = self.h_Altitudes[0:self.ts-1:1]
+            #             fillerL = self.velocitiesCompute*2 - self.ts
+            #             filler = numpy.multiply(numpy.ones(fillerL), self.h_Altitudes[0])
+            #             self.altitudes_HistMA = numpy.average(numpy.concatenate([filler, partialHistory]))
+            #     else:
+            #         # Update
+            #         self.altitudes_MA -= self.h_Altitudes[self.ts-self.velocitiesCompute]/self.velocitiesCompute
+            #         self.altitudes_MA += self.h_Altitudes[self.ts]/self.velocitiesCompute
+            #         if (self.ts - self.velocitiesCompute * 2 >= 0):
+            #             self.altitudes_HistMA -= self.h_Altitudes[self.ts-self.velocitiesCompute*2]/self.velocitiesCompute
+            #         else:
+            #             self.altitudes_HistMA -= self.h_Altitudes[0]/self.velocitiesCompute
+            #         self.altitudes_HistMA += self.h_Altitudes[self.ts-self.velocitiesCompute]/self.velocitiesCompute
+                
+            #     # Thrust
+            #     self.currentThrust = self.objectNeutralThrust - self.objectNeutralThrust / 10
+            #     if (self.stopDecel < 3):
+            #         if (self.altitudes_MA < self.altitudes_HistMA):
+            #             self.stopDecel -= 1
+            #             self.currentThrust = self.objectMaxThrust # Full decel
+            #         else:
+            #             self.stopDecel += 1
+
+        # end
+        self.ts += 1
+
+    @property
+    def heightFiltered(self):
+        '''
+        Tukaj vrnete trenutno trenutno višino ocenjeno z filtri.
+        To omogoča kasnejši izris, vrnete vašo interno spremenljivko.
+        '''
+        return self.currentAltitude
+
+    @property
+    def thrust(self):
+        '''
+        Tukaj vrnete relativno količino moči motorjev, vrednost med 0 in 1.
+        '''
+        return self.currentThrust
+    
+
---
+++ gravitySim2.py
@@ -0,0 +1,300 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Noise generator - do not touch!!!
+def awgn_noise_single_element(x, SNR_dB):
+    #np.random.seed(1)   # set the random generator seed to same value (for comparison only)
+    L = x.size
+    SNR = np.power(10, (SNR_dB/10))     # SNR to linear scale
+    Esym = np.sum(np.abs(x)**2) / (L)   # Calculate actual symbol energy
+    N0 = Esym / SNR                     # Find the noise spectral density
+    if np.isreal(x).all():
+        noiseSigma = np.sqrt(N0)        # Standard deviation for AWGN Noise when x is real
+        n = noiseSigma * np.random.randn(1, L)
+    else:
+        noiseSigma = np.sqrt(N0/2)      # Standard deviation for AWGN Noise when x is complex
+        n = noiseSigma * (np.random.randn(1, L) + complex(0, 1) * np.random.randn(1, L))
+
+    return n[-1, -1]
+
+class GravitySim():
+    '''
+    Ustvarimo nov simulator.
+
+    AutoLanderClass - razred AutoLander (ne objekt!), v katerem ste implementirali
+    lastno implementacijo pristajanja.
+
+    Parametri:
+    AutoLanderClass - razred, ki implemetira zahtevan vmesnik
+    timeDuration - čas trajanja simulacije
+    timeStep - korak simulacije
+    snr_dB - izbrana snr dB vrednost za šum, lahko ekperimentiramo
+    objectMass - float, masa objekta v kg
+    objectMaxThrust - maksimalni potisk motorjev, v N
+    startHeight - začetna višina simulacije v m
+    startVelocity - začetna hitrost v m/s (pozitivna je navzgor, negativna navzdol)
+    '''
+    def __init__(self,
+                 AutoLanderClass,
+                 animate=False,         # animate the plots
+                 timeDuration=10,       # seconds
+                 timeStep=0.001,        # seconds
+                 snr_dB=10.0,           # power of signal over noise
+                 objectMass=0.4,        # kg
+                 objectMaxThrust=11.06,  # F (force of engines)
+                 startHeight=20,         # meters (initial height)
+                 startVelocity=-3,       # meters / seconds (initial velocity)
+                 startThrust=0):        # percentage of maxThrust (initial force)
+
+        self.simTimeIndex = 0    # index of time step in simulation
+        self.AutoLanderClass = AutoLanderClass
+        self.autoLanderObj = None
+        self.animate = animate
+
+        ## simulation variables
+        self.timeDuration = timeDuration   # seconds
+        self.timeStep = timeStep # seconds
+
+        self.g = -9.81           # meters / seconds * seconds (gravitational change of velocity)
+        self.snr_dB = snr_dB # power of signal over noise
+
+        ## object variables
+
+        # properties
+        self.objectMass = objectMass
+        self.objectMaxThrust = objectMaxThrust
+        self.objectNeutralThrust = (self.objectMass * self.g / objectMaxThrust) * -1
+
+        # state
+        self.startHeight = startHeight # meters (initial height)
+        self.startVelocity = startVelocity  # meters / seconds (initial velocity)
+        self.startThrust = startThrust # percentage of maxThrust (initial force)
+
+        # history
+        self.historyTime = np.arange(self.timeStep,
+                                     self.timeDuration+self.timeStep,
+                                     self.timeStep)
+
+        self.historyHeight = np.zeros(self.historyTime.shape)
+        self.historyHeightWithNoise = np.zeros(self.historyTime.shape)
+        self.historyHeightWithFilter = np.zeros(self.historyTime.shape)
+
+        self.historyThrust = np.zeros(self.historyTime.shape)
+        self.historyForce = np.zeros(self.historyTime.shape)
+        self.historyVelocity = np.zeros(self.historyTime.shape)
+        self.historyEnergy = np.zeros(self.historyTime.shape)
+
+    def run(self):
+        '''
+        Pozene simulacijo, stopi skozi vse korake 
+        in sproti prikazuje rezultate (ce je animate=True).
+        '''
+        # prepare model
+        currentHeight = self.startHeight
+        currentVelocity = self.startVelocity
+        currentThrust = self.startThrust
+        currentEnergy = 0.5 * self.objectMass * np.power(currentVelocity, 2)
+
+        autoLanderObj = self.AutoLanderClass(self.objectMass,
+                                             self.objectMaxThrust,
+                                             self.objectNeutralThrust,
+                                             self.timeStep,
+                                             self.timeDuration,
+                                             self.snr_dB)
+        self.autoLanderObj = autoLanderObj
+
+        ts_last_update = 0
+        if self.animate:
+            self.animation_setup()
+
+        # simulation model loop
+        for ts in np.arange(1, self.historyTime.shape[0], 1):
+            ## prepare for new loop ...
+            previousHeight = currentHeight
+            previousVelocity = currentVelocity
+
+            # simulate noise :P
+            currentNoise = awgn_noise_single_element(self.historyHeight[0:ts+1], self.snr_dB)
+            currentHeight = currentHeight + currentNoise
+
+            autoLanderObj.addHeightMeasurement(self.historyTime[ts], currentHeight)
+            currentHeightFiltered = autoLanderObj.heightFiltered
+            self.historyHeightWithFilter[ts] = currentHeightFiltered
+            currentThrust = autoLanderObj.thrust
+
+            ## comply with controll restraints
+            if currentThrust < 0:
+                currentThrust = 0
+
+            if currentThrust > 1:
+                currentThrust = 1
+
+            self.historyThrust[ts] = currentThrust
+
+            ## calculate new state ...
+            # new force on object = force of gravity + current thrust
+            currentForce = (self.objectMass * self.g) + (currentThrust * self.objectMaxThrust)
+            self.historyForce[ts] = currentForce
+            # new velocity of object = previous velocity + force * time / mass
+            currentVelocity = previousVelocity + (currentForce * self.timeStep) / self.objectMass
+            self.historyVelocity[ts] = currentVelocity
+            # new displacement of object
+
+            #dH = previousVelocity*timeStep + 0.5*timeStep**2*currentAcc
+            #dH = (previousVelocity + currentVelocity) * 0.5 * self.timeStep
+            currentAcc = currentForce*self.timeStep/self.objectMass
+            dH = previousVelocity*self.timeStep + \
+                0.5 * currentAcc*self.timeStep**2
+            # new height of object
+            currentHeight = previousHeight + dH
+            self.historyHeight[ts] = currentHeight
+            self.historyHeightWithNoise[ts] = currentHeight + currentNoise
+            # new kinetic energy of object (or force at distance 0)
+            currentEnergy = 0.5 * self.objectMass * np.power(currentVelocity, 2)
+            self.historyEnergy[ts] = currentEnergy
+
+            if currentHeight <= 0:
+                self.simTimeIndex = ts
+                return
+
+            if self.historyTime[ts]-self.historyTime[ts_last_update] >= 1/30:
+                if self.animate:
+                    self.animation_update()
+                    ts_last_update = ts
+        self.simTimeIndex = -1
+
+    def printResult(self):
+        '''
+        Izpise koncne rezultate.
+        '''
+        ts = self.simTimeIndex
+        if ts > 0:
+            print('Simulation results:')
+            print('Landing time:', self.historyTime[ts], 's')
+            print('Landing speed:', self.historyVelocity[ts], 'm/s')
+            print('Landing force:', self.historyEnergy[ts], 'N')
+        else:
+            print('Simulation failed ...')
+
+
+    def animation_setup(self):
+        '''
+        setup animation frame
+        '''
+        plt.figure()
+
+        self.ax1 = plt.subplot(2, 2, 1)
+        plt.title('Height of object')
+        self.heightWithNoiseLine, = plt.plot(self.historyTime,
+                                             self.historyHeightWithNoise,
+                                             'r',
+                                             label='noise')
+        self.heightWithFilterLine, = plt.plot(self.historyTime,
+                                              self.historyHeightWithFilter,
+                                              'g',
+                                              label='noise - filter')
+        self.historyHeightLine, = plt.plot(self.historyTime,
+                                           self.historyHeight,
+                                           'b',
+                                           label='real')
+        plt.xlabel('time (s)'); plt.ylabel('height (m)')
+        plt.legend()
+
+        self.ax2 = plt.subplot(2, 2, 2)
+        self.axx2 = self.ax2.twinx()
+        plt.title('Force acted on object')
+        self.historyForceLine, = plt.plot(self.historyTime,
+                                          self.historyForce,
+                                          label='force')
+        self.historyThrustLine, = plt.plot(self.historyTime,
+                                           self.historyThrust,
+                                           label='thrust')
+        self.ax2.set_ylabel('thrust (%)')
+        self.axx2.set_ylabel('force (N)')
+        plt.xlabel('time (s)')
+        plt.legend()
+
+        self.ax3 = plt.subplot(2, 2, 3)
+        plt.title('Velocity of object')
+        self.historyVelocityLine, = plt.plot(self.historyTime,
+                                             self.historyVelocity)
+        plt.xlabel('time (s)'); plt.ylabel('velocity (m/s)')
+
+        self.ax4 = plt.subplot(2, 2, 4)
+        plt.title('Kinectic energy of object')
+        self.historyEnergyLine, = plt.plot(self.historyTime, self.historyEnergy)
+        plt.xlabel('time (s)'); plt.ylabel('energy (J)')
+
+        plt.show(block=False)
+        plt.waitforbuttonpress(0.01)
+
+    def animation_update(self):
+        '''
+        update animation frame
+        '''
+        self.heightWithNoiseLine.set_ydata(self.historyHeightWithNoise)
+        self.heightWithFilterLine.set_ydata(self.historyHeightWithFilter)
+        self.historyHeightLine.set_ydata(self.historyHeight)
+        self.ax1.relim()
+        self.ax1.autoscale_view()
+        self.historyForceLine.set_ydata(self.historyForce)
+        self.ax2.relim()
+        self.ax2.autoscale_view()
+        self.historyThrustLine.set_ydata(self.historyThrust)
+        self.axx2.relim()
+        self.axx2.autoscale_view()
+        self.historyVelocityLine.set_ydata(self.historyVelocity)
+        self.ax3.relim()
+        self.ax3.autoscale_view()
+        self.historyEnergyLine.set_ydata(self.historyEnergy)
+        self.ax4.relim()
+        self.ax4.autoscale_view()
+        plt.draw()
+        plt.waitforbuttonpress(0.01)
+
+
+    def plot(self):
+        '''
+        plot progress
+        '''
+        plt.figure()
+
+        plt.subplot(2, 2, 1)
+        plt.title('Height of object')
+        plt.plot(self.historyTime, self.historyHeightWithNoise, 'r', label='noise')
+        plt.plot(self.historyTime, self.historyHeightWithFilter, 'g', label='noise - filter')
+        plt.plot(self.historyTime, self.historyHeight, 'b', label='real')
+        plt.xlabel('time (s)'); plt.ylabel('height (m)')
+        plt.legend()
+
+        ax = plt.subplot(2, 2, 2)
+        axx = ax.twinx()
+        plt.title('Force acted on object')
+        plt.plot(self.historyTime, self.historyForce, label='force')
+        plt.plot(self.historyTime, self.historyThrust, label='thrust')
+        ax.set_ylabel('thrust (%)')
+        axx.set_ylabel('force (N)')
+        plt.xlabel('time (s)')
+        plt.legend()
+
+        plt.subplot(2, 2, 3)
+        plt.title('Velocity of object')
+        plt.plot(self.historyTime, self.historyVelocity)
+        plt.xlabel('time (s)'); plt.ylabel('velocity (m/s)')
+
+        plt.subplot(2, 2, 4)
+        plt.title('Kinectic energy of object')
+        plt.plot(self.historyTime, self.historyEnergy)
+        plt.xlabel('time (s)'); plt.ylabel('energy (J)')
+
+        plt.show()
+
+from autolander import AutoLander
+
+if __name__ == '__main__':
+    grav_sim = GravitySim(AutoLander,
+                          animate=False)
+
+    grav_sim.run()
+    grav_sim.printResult()
+    grav_sim.plot()
